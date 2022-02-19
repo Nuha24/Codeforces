8b541ade17cce778866aa3a5a69922d659a426e5
Maksura Nuha <94732532+Nuha24@users.noreply.github.com>
2022-02-19 11:25:32 +0600

    Add files via upload

--- CF sheet 1.c
+++ CF sheet 1.c
@@ -1,1 +1,11 @@
-
+//C-
+#include <stdio.h>
+int main()
+{
+   long long int X,Y;
+   scanf("%lld %lld",&X,&Y);
+   printf("%lld + %lld = %lld\n",X,Y,(X+Y));
+   printf("%lld * %lld = %lld\n",X,Y,(X*Y));
+   printf("%lld - %lld = %lld\n",X,Y,(X-Y));
+   return 0;
+}
