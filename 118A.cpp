2f60432e4fb466c9187b8803250ae7daf54ec4a4
Nuha <94732532+Nuha24@users.noreply.github.com>
2022-02-20 11:37:19 +0600

    Update 118A.cpp
    118A - String Task
    https://codeforces.com/problemset/problem/118/A

--- 118A.cpp
+++ 118A.cpp
@@ -1,1 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    int i,l;
+    l= s.size();
+    for (i=0; i<l; i++)
+    {
+        char x=tolower(s[i]);
+        if (x=='a' || x=='e' || x=='i' || x=='o' || x=='u' || x=='y')
+            continue;
+
+        else
+        {
+            cout<<"."<<x;
+        }
+    }
+}
 
